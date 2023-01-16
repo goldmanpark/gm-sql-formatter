286e0fc07df9673118baf238a056411cc43f064a
Goldman Park <death2501@gmail.com>
2023-01-16 11:00:23 +0900

    README, Function args관련 수정

--- src/Elements/expressions/Function.ts
+++ src/Elements/expressions/Function.ts
@@ -18,33 +18,35 @@ export class Function implements Element
         this.name = func.name.toUpperCase();
         this.alias = null;
 
-        if(func.args.value instanceof Array && func.args.value.length > 0)
-        {
-            this.content = func.args.value.map((param: any) =>
-            {
-                switch (param.type) 
-                {
-                    case 'star':
-                        return '*';
-                    case 'var':
-                        return param.prefix + param.name;
-                    case 'function':
-                        return new Function(param, this.depth + 1);
-                    case 'single_quote_string':
-                        return "'" + param.value.toString() + "'";
-                    case 'column_ref':
-                        return param.table ? param.table + '.' + param.column : param.column;
-                    case 'number':
-                        return param.value.toString();
-                    default:
-                        break;
-                }
-            });
-        }
-        else if(func.args.expr && func.args.expr.type === 'case')
-            this.content = new CASE(func.args.expr.args, this.depth + 1);
+        if(func.args.type === 'expr_list')
+            this.content = func.args.value.map((x: any) => { return this.createParam(x); });
+        else if(func.args.expr)
+            this.content = [this.createParam(func.args.expr)];
         else
             this.content = null;
+    }
+
+    createParam(expr: any): string | CASE | Function | Expression | Statement
+    {
+        switch (expr.type) 
+        {
+            case 'star':
+                return '*';
+            case 'var':
+                return expr.prefix + expr.name;
+            case 'function':
+                return new Function(expr, this.depth + 1);
+            case 'single_quote_string':
+                return "'" + expr.value.toString() + "'";
+            case 'column_ref':
+                return expr.table ? expr.table + '.' + expr.column : expr.column;
+            case 'number':
+                return expr.value.toString();
+            case 'case':
+                return new CASE(expr.args, this.depth + 1);
+            default:
+                return '';
+        }
     }
 
     getSQL(): string
